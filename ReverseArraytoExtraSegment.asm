fe23d952f4775dadf2e9220725ccf5d9e5112059
Muhammet <muhammetgms95@gmail.com>
2018-12-14 11:52:27 +0300

    Add files via upload

---
+++ ReverseArraytoExtraSegment.asm
@@ -0,0 +1,26 @@
+ 
+;Our goal is reverse the given array and store them onto ES.
+ 
+org 100h
+  
+lea si,array
+mov ax,5000h
+mov es,ax
+mov di,1103H
+
+
+mov cx,6
+;mov ax,0
+   L1:
+     CLD     
+     lodsb
+     STD
+     stosb
+   loop L1
+
+ret
+
+array DB '1','2','3','4','5','6'
+
+
+
